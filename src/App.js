8c66d1fa87d79a9999d9150a5bdd3f3a3dccd662
Jack <jacktiebie45@gmail.com>
2022-04-18 12:00:33 +0200

    JokeMap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Joke from './components/Joke'
+import jokesData from './jokesData'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const jokeElements = jokesData.map(joke => {
+        return <Joke 
+        question={joke.question}
+        answer={joke.answer}
+        />
+    })
+    return (
+        <div>
+        {jokeElements}
+        </div>
+    )
 }
 
-export default App;
+/*
+  <Joke 
+            question="Why did the carrot cross the street?"
+            answer="Because he was orange!"
+           />
+            <Joke 
+            question="What do you call a fish without eyes?"
+            answer="Fsh"
+            />
+            <Joke 
+            question="Why did the scarecrow win an award?"
+            answer="Because he was outstanding in his field."
+            />
+            <Joke 
+             question="What lights up a soccer stadium?"
+             answer="A soccer match."
+            />
+            <Joke
+             question="Why shouldn’t you write with a broken pencil?"
+             answer="Because it’s pointless."
+            />
+*/
---
+++ src/jokesData.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        question: "Why shouldn’t you write with a broken pencil?",
+        answer: "Because it’s pointless."
+    },
+    {
+        question: "What lights up a soccer stadium?",
+        answer: "A soccer match."
+    },
+    {
+        question: "Why did the scarecrow win an award?",
+        answer: "Because he was outstanding in his field."
+    },
+    {
+        question: "What do you call a fish without eyes?",
+        answer: "Fsh"
+    },
+    {
+        question: "Why did the carrot cross the street?",
+        answer: "Because he was orange!"
+    }
+]
