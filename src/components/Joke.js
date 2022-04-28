c3e2eaa04d8d558460b6b386d01056e121a875a0
Jack <jacktiebie45@gmail.com>
2022-04-28 11:36:53 +0200

    code

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
 export default function Joke(props) {
+const [isShown, setIsShown] = useState(false)
+
+function toggle() {
+    setIsShown(prevShown => !prevShown)
+}
+
     return (
         <div className='space'>
-        <p>{props.question}</p>
-        <p>{props.answer}</p>
+        {props.question && <h3>{props.question}</h3>}
+        {isShown && <p>{props.answer}</p>}
+        <button onClick={toggle}>{isShown ? 'Hide Punchline' : 'Show Punchline'}</button>
         <hr/>
         </div>
+        
     )
 }
